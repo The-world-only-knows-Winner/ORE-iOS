a64fbdad51d7ab55d1abe46ec7325a4bcc8f192f
hsj._.06 <gtw030488@gmail.com>
2023-09-02 08:44:36 +0900

    faet :: SPM

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -1,3 +1,4 @@
+
 import ProjectDescription
 
 public extension TargetDependency {
@@ -5,6 +6,11 @@ public extension TargetDependency {
 }
 
 public extension TargetDependency.SPM {
+    static let Quick = TargetDependency.external(name: "Quick")
+    static let Nimble = TargetDependency.external(name: "Nimble")
+    static let Needle = TargetDependency.external(name: "NeedleFoundation")
+    static let Moya = TargetDependency.external(name: "Moya")
+    static let CombineMoya = TargetDependency.external(name: "CombineMoya")
 }
 
 public extension Package {
--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -5,7 +5,10 @@ let dependencies = Dependencies(
     carthage: nil,
     swiftPackageManager: SwiftPackageManagerDependencies(
         [
-    
+            .remote(url: "https://github.com/Quick/Quick.git", requirement: .upToNextMajor(from: "5.0.1")),
+            .remote(url: "https://github.com/Quick/Nimble.git", requirement: .upToNextMajor(from: "10.0.0")),
+            .remote(url: "https://github.com/uber/needle.git", requirement: .upToNextMajor(from: "0.19.0")),
+            .remote(url: "https://github.com/team-aliens/Moya.git", requirement: .branch("master"))
         ],
         baseSettings: .settings(
             configurations: [
