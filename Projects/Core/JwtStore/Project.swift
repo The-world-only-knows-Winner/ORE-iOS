405c0038bc67489e0167279a21ecc7b42ca6f295
hsj._.06 <gtw030488@gmail.com>
2023-09-03 00:33:00 +0900

    feat :: jwt dependency

--- Projects/Core/JwtStore/Project.swift
+++ Projects/Core/JwtStore/Project.swift
@@ -5,8 +5,13 @@ import DependencyPlugin
 let project = Project.module(
     name: ModulePaths.Core.JwtStore.rawValue,
     targets: [
+        .interface(module: .core(.JwtStore)),
         .implements(module: .core(.JwtStore), dependencies: [
+            .core(target: .JwtStore, type: .interface),
             .SPM.Moya
+        ]),
+        .tests(module: .core(.JwtStore), dependencies: [
+            .core(target: .JwtStore)
         ])
     ]
 )
