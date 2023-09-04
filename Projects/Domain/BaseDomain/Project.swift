aff931b07dc2533408f5f4370a869a2628f618ec
hsj._.06 <gtw030488@gmail.com>
2023-09-04 16:39:34 +0900

    feat :: auth domain

--- Projects/Domain/BaseDomain/Project.swift
+++ Projects/Domain/BaseDomain/Project.swift
@@ -1,15 +1,19 @@
-import DependencyPlugin
 import ProjectDescription
 import ProjectDescriptionHelpers
+import DependencyPlugin
 
 let project = Project.module(
     name: ModulePaths.Domain.BaseDomain.rawValue,
     targets: [
-        .implements(module: .domain(.BaseDomain), dependencies: [
-            .shared(target: .GlobalThirdPartyLibrary)
+        .implements(module: .domain(.BaseDomain), product: .framework, dependencies: [
+            .SPM.Moya,
+            .SPM.CombineMoya,
+            .core(target: .JwtStore, type: .interface),
+            .shared(target: .GlobalThirdPartyLibrary),
+            .shared(target: .UtilityModule)
         ]),
         .tests(module: .domain(.BaseDomain), dependencies: [
-            .domain(target: .BaseDomain, type: .interface)
+            .domain(target: .BaseDomain)
         ])
     ]
 )
