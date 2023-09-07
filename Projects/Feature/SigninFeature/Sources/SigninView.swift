fe2377969e8b707654437e653652420673d79eed
hsj._.06 <gtw030488@gmail.com>
2023-09-07 21:39:18 +0900

    feat :: 로그인 뷰 퍼블리싱

--- Projects/Feature/SigninFeature/Sources/SigninView.swift
+++ Projects/Feature/SigninFeature/Sources/SigninView.swift
@@ -3,6 +3,12 @@ import SwiftUI
 import BaseFeature
 
 struct SigninView: View {
+    private enum FocusField {
+        case email
+        case password
+    }
+    @FocusState private var focusField: FocusField?
+    @Environment(\.dismiss) private var dismiss
     @EnvironmentObject var appState: AppState
     @StateObject var viewModel: SigninViewModel
 
@@ -13,6 +19,42 @@ struct SigninView: View {
     }
 
     var body: some View {
-        Text("Signin View")
+        VStack(alignment: .leading, spacing: 0) {
+            ORITextField(
+                text: $viewModel.email,
+                placehoder: "이메일을 입력해주세요",
+                title: "이메일",
+                description: viewModel.emailDescription
+            ) {
+                focusField = .password
+            }
+            .keyboardType(.emailAddress)
+            .focused($focusField, equals: .email)
+
+            ORITextField(
+                text: $viewModel.password,
+                placehoder: "비밀번호을 입력해주세요",
+                title: "비밀번호",
+                type: .secure
+            ) {
+                // login!
+            }
+            .textContentType(.password)
+            .focused($focusField, equals: .password)
+
+            Spacer()
+
+            ORIButton(text: "시작하기", isFocused: focusField != .none) {
+                // login
+            }
+        }
+        .onAppear {
+            focusField = .email
+        }
+        .hideKeyboardWhenTap()
+        .oriNavigationBar(
+            leadingItem: NavigationItem(image: ORIIcon(.arrowBack)) { dismiss() },
+            pageTitle: "다시 오신걸 환영해요!"
+        )
     }
 }
--- Projects/Feature/SigninFeature/Sources/SigninViewModel.swift
+++ Projects/Feature/SigninFeature/Sources/SigninViewModel.swift
@@ -1,6 +1,11 @@
 import Foundation
 import BaseFeature
 import Combine
+import DesignSystem
 
 final class SigninViewModel: BaseViewModel {
+    @Published var email: String = ""
+    @Published var emailDescription: DescriptionType?
+
+    @Published var password: String = ""
 }
