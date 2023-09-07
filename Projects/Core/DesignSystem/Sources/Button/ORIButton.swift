625dee2cd5e6b618fe5d9a2d3f3a8017152b8880
hsj._.06 <gtw030488@gmail.com>
2023-09-07 13:19:51 +0900

    feaet :: icon svg로 교체

---
+++ Projects/Core/DesignSystem/Sources/Button/ORIButton.swift
@@ -0,0 +1,9 @@
+//
+//  ORIButton.swift
+//  DesignSystem
+//
+//  Created by 홍승재 on 2023/09/07.
+//  Copyright © 2023 com.onlywin. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Core/DesignSystem/Sources/Button/ORIButtonStyle.swift
@@ -0,0 +1,9 @@
+//
+//  ORIButtonStyle.swift
+//  DesignSystem
+//
+//  Created by 홍승재 on 2023/09/07.
+//  Copyright © 2023 com.onlywin. All rights reserved.
+//
+
+import Foundation
