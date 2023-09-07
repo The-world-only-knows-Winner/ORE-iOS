85c122fde2ff6929bbb5281131aeb8723175da85
hsj._.06 <gtw030488@gmail.com>
2023-09-07 13:20:03 +0900

    feat :: primary Color 추가

--- Projects/Core/DesignSystem/Sources/Color/ORIPrimaryColor.swift
+++ Projects/Core/DesignSystem/Sources/Color/ORIPrimaryColor.swift
@@ -6,5 +6,7 @@ public extension Color {
 
 public extension Color.Primary {
     static let primary100: Color = DesignSystemAsset.Primary.primary100.swiftUIColor
+    static let primary200: Color = DesignSystemAsset.Primary.primary200.swiftUIColor
     static let primary300: Color = DesignSystemAsset.Primary.primary300.swiftUIColor
+    static let primary400: Color = DesignSystemAsset.Primary.primary400.swiftUIColor
 }
