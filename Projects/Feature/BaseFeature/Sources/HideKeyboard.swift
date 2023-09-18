5a8d82f45e5acb708d17fb99f732451f2ca51aec
hsj._.06 <gtw030488@gmail.com>
2023-09-18 09:07:26 +0900

    feat :: hideKeyboard baseFeature -> UtilityModule

--- Projects/Feature/BaseFeature/Sources/HideKeyboard.swift
+++ Projects/Feature/BaseFeature/Sources/HideKeyboard.swift
@@ -2,10 +2,6 @@ import SwiftUI
 import UIKit
 
 public extension View {
-    func hideKeyboard() {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
-
     func hideKeyboardWhenTap() -> some View {
         onAppear(perform: UIApplication.shared.hideKeyboard)
     }
