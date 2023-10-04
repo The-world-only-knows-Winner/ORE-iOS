749d3d4203777e3513afdf5ae045f5b3eeef6426
hsj._.06 <gtw030488@gmail.com>
2023-10-04 20:06:17 +0900

    feat :: ConfirmView

--- Projects/Feature/HomeFeature/Sources/Confirm/ConfirmViewModel.swift
+++ Projects/Feature/HomeFeature/Sources/Confirm/ConfirmViewModel.swift
@@ -1,6 +1,8 @@
 import BaseFeature
 import Combine
 
-final class RouteConfirmViewModel: BaseViewModel {
+final class ConfirmViewModel: BaseViewModel {
     @Published var isNavigatedToSetAlarm = false
+    @Published var isNavigatedToConfirmRoute = false
+    @Published var isNavigatedToSuccessSelectRoute = false
 }
