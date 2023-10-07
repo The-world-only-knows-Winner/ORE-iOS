a6ccb0dc6cd12241daf756d85974c0d4a35dd061
hsj._.06 <gtw030488@gmail.com>
2023-10-07 11:06:24 +0900

    feat :: Repository

--- Projects/Domain/AuthDomain/Sources/Repository/AuthRepositoryImpl.swift
+++ Projects/Domain/AuthDomain/Sources/Repository/AuthRepositoryImpl.swift
@@ -3,29 +3,30 @@ import Combine
 
 public struct AuthRepositoryImpl: AuthRepository {
     private let remoteAuthDataSource: any RemoteAuthDataSource
-    private let localAuthDataSource: any LocalAuthDataSource
 
     public init(
-        remoteAuthDataSource: any RemoteAuthDataSource,
-        localAuthDataSource: any LocalAuthDataSource
+        remoteAuthDataSource: any RemoteAuthDataSource
     ) {
         self.remoteAuthDataSource = remoteAuthDataSource
-        self.localAuthDataSource = localAuthDataSource
+    }
+
+    public func signin(req: SigninRequestDTO) -> AnyPublisher<Void, Error> {
+        remoteAuthDataSource.signin(req: req)
+    }
+
+    public func changePassword(req: ChangePasswordRequestDTO) -> AnyPublisher<Void, Error> {
+        remoteAuthDataSource.changePassword(req: req)
     }
 
     public func sendAuthCode(req: SendAuthCodeRequestDTO) -> AnyPublisher<Void, Error> {
         remoteAuthDataSource.sendAuthCode(req: req)
     }
 
-    public func reissueToken() -> AnyPublisher<Bool, Error> {
-        remoteAuthDataSource.reissueToken()
+    public func verifyAuthCode(req: VerifyAuthCodeRequestDTO) -> AnyPublisher<Void, Error> {
+        remoteAuthDataSource.verifyAuthCode(req: req)
     }
 
-    public func verifyAuthCode(email: String, authCode: String) -> AnyPublisher<Void, Error> {
-        remoteAuthDataSource.verifyAuthCode(email: email, authCode: authCode)
-    }
-
-    public func logout() {
-        localAuthDataSource.logout()
+    public func logout() -> AnyPublisher<Void, Error> {
+        remoteAuthDataSource.logout()
     }
 }
