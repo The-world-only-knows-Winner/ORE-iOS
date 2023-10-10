44ad8d21de765c69fd10190405f786570abb2b2d
hsj._.06 <gtw030488@gmail.com>
2023-10-11 08:50:21 +0900

    feat :: 비밀번호 변경 api

--- Projects/Feature/RenewalPasswordFeature/Sources/RenewalPasswordComponent.swift
+++ Projects/Feature/RenewalPasswordFeature/Sources/RenewalPasswordComponent.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 import NeedleFoundation
 import RenewalPasswordFeatureInterface
+import AuthDomainInterface
 
-public protocol RenewalPasswordDependency: Dependency {}
+public protocol RenewalPasswordDependency: Dependency {
+    var changePasswordUseCase: any ChangePasswordUseCase { get }
+}
 
 public final class RenewalPasswordComponent: Component<RenewalPasswordDependency>, RenewalPasswordFactory {
     public func makeView() -> some View {
-        RenewalPasswordView(viewModel: .init())
+        RenewalPasswordView(viewModel: .init(changePasswordUseCase: dependency.changePasswordUseCase))
     }
 }
