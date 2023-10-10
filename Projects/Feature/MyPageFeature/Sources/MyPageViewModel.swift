64ebc71be242e00d3c1810da5564385b2a7fe098
hsj._.06 <gtw030488@gmail.com>
2023-10-11 08:50:29 +0900

    feat :: MyPage api

--- Projects/Feature/MyPageFeature/Sources/MyPageViewModel.swift
+++ Projects/Feature/MyPageFeature/Sources/MyPageViewModel.swift
@@ -1,7 +1,8 @@
 import BaseFeature
+import AuthDomainInterface
 import Combine
 import SwiftUI
-import AuthDomainInterface
+import UserDomainInterface
 
 final class MyPageViewModel: BaseViewModel {
     @Published var isNotificationsActive = true
@@ -10,4 +11,35 @@ final class MyPageViewModel: BaseViewModel {
     @Published var isNavigatedToRenewalPassword = false
     @Published var isShowingLogoutAlert = false
     @Published var isNavigatedToLogout = false
+    @Published var myInfo: MyInfoEntity?
+
+    private let fetchMyInfoUseCase: any FetchMyInfoUseCase
+    private let logoutUseCase: any LogoutUseCase
+
+    init(
+        fetchMyInfoUseCase: any FetchMyInfoUseCase,
+        logoutUseCase: any LogoutUseCase
+    ) {
+        self.fetchMyInfoUseCase = fetchMyInfoUseCase
+        self.logoutUseCase = logoutUseCase
+    }
+
+    func fetchMyInfo() {
+        addCancellable(
+            fetchMyInfoUseCase.execute()
+        ) { [weak self] myInfo in
+            self?.myInfo = MyInfoEntity(
+                name: myInfo.name,
+                birthday: myInfo.birthday.replacingOccurrences(of: "-", with: ".")
+            )
+        }
+    }
+
+    func logout() {
+        addCancellable(
+            logoutUseCase.execute()
+        ) { [weak self] _ in
+            self?.isNavigatedToLogout = true
+        }
+    }
 }
